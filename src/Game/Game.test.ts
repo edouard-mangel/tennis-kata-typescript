2fe7e2b31b707966a1687233057f5983b188ddbf
Albert Ricart <albert.ricart@ext.arte.tv>
2026-07-09 13:34:32 +0200

    feat: player1 can score twice

--- src/Game/Game.test.ts
+++ src/Game/Game.test.ts
@@ -1,16 +1,19 @@
 import { expect, test } from 'vitest'
 
 class Game {
-    private constructor(private player1Score: number, private player2Score: number){
+    private constructor(private player1Score: number, private player2Score: number) {
     }
 
-    public static NewGame():Game{
-        return new Game(0,0);
+    public static NewGame(): Game {
+        return new Game(0, 0);
     }
 
     scoreToDisplay(): string {
         if (this.player1Score > 0 && this.player2Score > 0) {
             return '15-15';
+        }
+        if (this.player1Score > 1) {
+            return '30-0';
         }
         if (this.player1Score > 0) {
             return '15-0';
@@ -21,7 +24,7 @@ class Game {
         return '0-0';
     }
 
-    player1Scored() : Game{
+    player1Scored(): Game {
         return new Game(++this.player1Score, this.player2Score);
     }
 
@@ -44,6 +47,10 @@ test('a game with player 2 who scored once should display 0 - 15', () => {
 
 test('a game with both players who scored once should display 15 - 15', () => {
     expect((Game.NewGame()
-            .player1Scored()
-            .player2Scored()).scoreToDisplay()).toBe('15-15');
+        .player1Scored()
+        .player2Scored()).scoreToDisplay()).toBe('15-15');
+});
+
+test('a game with player 1 who scored twice should display 30 - 0', () => {
+    expect(Game.NewGame().player1Scored().player1Scored().scoreToDisplay()).toBe('30-0');
 });
