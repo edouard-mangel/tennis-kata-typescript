437d8379328e303eaa9e715e298311e30e67053a
Albert Ricart <albert.ricart@ext.arte.tv>
2026-07-09 13:13:52 +0200

    feat: introduce player1Scored function

--- src/Game/Game.test.ts
+++ src/Game/Game.test.ts
@@ -1,9 +1,21 @@
 import { expect, test } from 'vitest'
 
 class Game {
-    scoreToDisplay() {
+    player1Score: number = 0;
+
+
+
+    scoreToDisplay(): string {
+        if (this.player1Score > 0) {
+            return '15-0';
+        }
         return '0-0';
     }
+
+    player1Scored() {
+        this.player1Score++;
+    }
+
 }
 
 test('a new game shoud have a nil score to display', () => {
@@ -11,3 +23,17 @@ test('a new game shoud have a nil score to display', () => {
     // Le jeu initialisé a comme score à afficher 0 - 0
     expect(new Game().scoreToDisplay()).toBe('0-0');
 })
+
+test('TODO rename', () => {
+    // Given
+    // Un jeu initialise
+    const game = new Game();
+
+    // When
+    // Quand le joueur 1 marque un point
+    game.player1Scored();
+
+    // Then
+    // Le score à afficher doit être 15-0
+    expect(game.scoreToDisplay()).toBe('15-0');
+});
