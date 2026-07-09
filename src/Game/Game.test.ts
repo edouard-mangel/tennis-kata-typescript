afa7c75699c5a10c4063ba9691465d71316dc5be
Edouard MANGEL <edouard@lacrafterie.tech>
2026-07-09 13:27:49 +0200

    refactor: make Game immutable

--- src/Game/Game.test.ts
+++ src/Game/Game.test.ts
@@ -1,10 +1,12 @@
 import { expect, test } from 'vitest'
 
 class Game {
-    private player1Score: number = 0;
-    private player2Score: number = 0;
+    private constructor(private player1Score: number, private player2Score: number){
+    }
 
-
+    public static NewGame():Game{
+        return new Game(0,0);
+    }
 
     scoreToDisplay(): string {
         if (this.player1Score > 0 && this.player2Score > 0) {
@@ -19,29 +21,25 @@ class Game {
         return '0-0';
     }
 
-    player1Scored() {
-        this.player1Score++;
+    player1Scored() : Game{
+        return new Game(++this.player1Score, this.player2Score);
     }
 
     player2Scored() {
-        this.player2Score++;
+        return new Game(this.player1Score, ++this.player2Score);
     }
 }
 
 test('a new game shoud have a nil score to display', () => {
     // Assert / Then
     // Le jeu initialisé a comme score à afficher 0 - 0
-    expect(new Game().scoreToDisplay()).toBe('0-0');
+    expect(Game.NewGame().scoreToDisplay()).toBe('0-0');
 })
 
 test('a game with player 1 who scored once should display 15 - 0', () => {
     // Given
-    // Un jeu initialise
-    const game = new Game();
-
-    // When
-    // Quand le joueur 1 marque un point
-    game.player1Scored();
+    // Un jeu où le joueur 1 marque un point
+    const game = Game.NewGame().player1Scored();
 
     // Then
     // Le score à afficher doit être 15-0
@@ -50,23 +48,19 @@ test('a game with player 1 who scored once should display 15 - 0', () => {
 
 test('a game with player 2 who scored once should display 0 - 15', () => {
     // Given
-    const game = new Game();
-
-    // When
-    game.player2Scored();
+    const game = Game.NewGame().player2Scored();
 
     // Then
     expect(game.scoreToDisplay()).toBe('0-15');
 });
 
 test('a game with both players who scored once should display 15 - 15', () => {
-    // Given
-    const game = new Game();
-
-    // When
-    game.player1Scored();
-    game.player2Scored();
+    // A game where both players scored once. 
+    const game = Game.NewGame()
+                    .player1Scored()
+                    .player2Scored();
 
     // Then
     expect(game.scoreToDisplay()).toBe('15-15');
 });
+
